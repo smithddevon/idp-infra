f8df476100f235b228e926ef6b7ade3be40522f7
Devon Smith <dsmith418@icloud.com>
2026-01-30 20:13:00 -0500

    Changed node size

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     node_config {
         machine_type = "e2-medium"
+        disk_size_gb = 30
         oauth_scopes = [
                  "https://www.googleapis.com/auth/cloud-platform",
         ]      
