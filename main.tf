2dcac261b06012ac39973c078751e96da43fb425
Devon Smith <dsmith418@icloud.com>
2026-01-30 21:26:58 -0500

    Added disk type

--- main.tf
+++ main.tf
@@ -38,6 +38,8 @@ resource "google_container_node_pool" "primary_nodes" {
 
     node_config {
         machine_type = "e2-medium"
+        disk_type    
+        = "pd-standard"
         disk_size_gb = 30
         oauth_scopes = [
                  "https://www.googleapis.com/auth/cloud-platform",
